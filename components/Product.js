4ae95abf911366127c763fc61ea99df3b01dac82
suprsidr <suprsidr@gmail.com>
2016-04-23 06:55:26 -0500

    less margin under thumbnail

--- components/Product.js
+++ components/Product.js
@@ -11,7 +11,7 @@ export class Product extends Component {
   render() {
     return (
       <div className="column text-center">
-        <p>
+        <p className="prodThumb">
           <Link to={`/product/${this.props.product.ProdID}`}>
             <img src={`http://s7d5.scene7.com/is/image/horizonhobby/${this.props.product.ProdID}_a0?wid=300&hei=300`} onError={(e) => this.errorHandler(e, 300)} className="thumbnail" alt="" />
           </Link>
