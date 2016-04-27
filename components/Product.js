8caeadde9fff3618ee69d088a29e72c96e52ae4a
Wayne Patterson <suprsidr@gmail.com>
2016-04-27 05:41:20 -0500

    add media

--- components/Product.js
+++ components/Product.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import { Link } from 'react-router';
 import ProdPriceBox from './ProdPriceBox';
 import ProdTabs from './ProdTabs';
+import ProdMedia from './ProdMedia';
 
 export class Product extends Component {
 	errorHandler(e, wid) {
@@ -30,12 +31,15 @@ export class ProdPageProduct extends Component {
           <div className="small-12 columns prodTitle">
             <h3>{this.props.product.Name.replace(/&reg;/g, '')}</h3><strong>{`(${this.props.product.ProdID})`}</strong>
           </div>
-          <div className="small-12 medium-7 large-8 columns">
+          <div className="small-12 medium-7 large-8 columns text-center">
             <p><img src={`http://s7d5.scene7.com/is/image/horizonhobby/${this.props.product.ProdID}_a0?wid=500`} onError={(e) => this.errorHandler(e, 500)} className="hero" alt={this.props.product.Name.replace(/&reg;/g, '')} /></p>
           </div>
-          <div className="small-12 medium-5 large-4 columns">
+          <div className="small-12 medium-5 large-4 columns text-center">
             <ProdPriceBox product={this.props.product} />
           </div>
+	        <div className="small-12 columns">
+		        <ProdMedia product={this.props.product} />
+	        </div>
         </div>
         <ProdTabs product={this.props.product} db={this.props.db} />
       </div>
