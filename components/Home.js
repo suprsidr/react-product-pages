f93392bafa1f032565a15c8e9d065a2f35bb442a
suprsidr <suprsidr@gmail.com>
2016-04-29 13:37:09 -0500

    update description

--- components/Home.js
+++ components/Home.js
@@ -6,7 +6,24 @@ export default class Home extends Component {
       <div className="row">
         <div className="small-12 columns">
           <h2>Brand Site POC</h2>
-          <p>Setting up a possible future for brand sites that would allow us to take down Nexus Yay!!!</p>
+	        <p>The entire business logic of this site is in ~130K of javascript.</p>
+	        <p>I'm making one single call to the REST API on Digital Ocean to bootstrap the application. I store the data in a browser version of MongoDB(minimongo).</p>
+	        <p>All product display/search results are from queries of minimongo.</p>
+	        <p>If a product is not found in minimongo I query the REST API for the data for that product and add it to minimongo to be available for next time it is needed. Mostly only necessary for parts lists.</p>
+	        <p>&nbsp;</p>
+          <p>This would be the site's homepage.</p>
+	        <p>There would likely be a slider and any other relevent marketing.</p>
+	        <p>And also social media feeds/links.</p>
+	        <p>I chose Blade for this demo, and provided a couple of category links in the header. But any brand would be simple.</p>
+	        <p>I've roughed out most functionality. But am taking requests for further functionality.</p>
+	        <p>TODOS:
+	          <ul>
+		          <li>Make a presentable homepage</li>
+		          <li>Tweak mobile styles a bit</li>
+		          <li>Make product page slideshow more user friendly</li>
+		          <li>Integrate jQuery - Everything so far is without jQuery.<br />But it would be useful for some stuff like Foundation.</li>
+	          </ul>
+	        </p>
         </div>
       </div>
     )
