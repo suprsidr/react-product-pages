d5f6e2010f791c14ae8a2443ce73a2760d9035a1
suprsidr <suprsidr@gmail.com>
2016-04-30 14:36:53 -0500

    trying to work in react-foundation

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,20 @@
 import React, {Component} from 'react';
 import {Link, IndexLink, browserHistory} from 'react-router';
+import {
+		ResponsiveNavigation,
+		TopBarTitle,
+		TopBarLeft,
+		TopBarRight,
+		Menu,
+		MenuItem
+} from 'react-foundation';
 
 class Header extends Component {
-	toggleMenu(e) {
+	/*toggleMenu(e) {
 		e.preventDefault();
 		const menu = document.getElementById('headerMenu');
 		menu.style.display = (menu.style.display === 'none' || menu.style.display === '') ? 'block' : 'none';
-	}
+	}*/
 	handleSubmit(e) {
 		e.preventDefault();
 		const val = this.refs.search.value || '';
@@ -20,34 +28,28 @@ class Header extends Component {
 	}
   render() {
     return (
-		    <div>
-			    <div className="title-bar" data-responsive-toggle="headerMenu" data-hide-for="medium">
-				    <button className="menu-icon" type="button" data-toggle onClick={(e) => this.toggleMenu(e)}></button>
-				    <div className="title-bar-title">Menu</div>
-			    </div>
-		      <div className="top-bar" role="nav" id="headerMenu">
-		        <div className="top-bar-left">
-		          <ul className="menu">
-		            <li className="menu-text hide-for-small-only">POC Brand Site</li>
-		            <li><IndexLink to="/">Home</IndexLink></li>
-		            <li><Link to="/products/multirotor" activeStyle={{ color: '#00d8ff' }}>MultiRotor</Link></li>
-		            <li><Link to="/products/helicopters" activeStyle={{ color: '#00d8ff' }}>Helicopters</Link></li>
-		          </ul>
-		        </div>
-			      <div className="top-bar-right">
-				      <ul className="menu">
-					      <li><input ref="search" type="search" placeholder="Search" onKeyUp={(e) => this.handleKeyup(e)} /></li>
-					      <li><button type="button" className="button" onClick={(e) => this.handleSubmit(e)}>Search</button></li>
-				      </ul>
-			      </div>
-		      </div>
-		    </div>
+	    <ResponsiveNavigation className="navbar">
+		    <TopBarTitle>POC Brand Site</TopBarTitle>
+		    <TopBarLeft>
+			    <Menu>
+				    <MenuItem><IndexLink to="/">Home</IndexLink></MenuItem>
+				    <MenuItem><Link to="/products/multirotor" activeStyle={{ color: '#00d8ff' }}>MultiRotor</Link></MenuItem>
+				    <MenuItem><Link to="/products/helicopters" activeStyle={{ color: '#00d8ff' }}>Helicopters</Link></MenuItem>
+			    </Menu>
+		    </TopBarLeft>
+		    <TopBarRight>
+			    <Menu>
+				    <MenuItem>
+					    <input ref="search" type="search" placeholder="Search" onKeyUp={(e) => this.handleKeyup(e)} />
+				    </MenuItem>
+				    <MenuItem>
+					    <button type="button" className="button" onClick={(e) => this.handleSubmit(e)}>Search</button>
+				    </MenuItem>
+			    </Menu>
+		    </TopBarRight>
+	    </ResponsiveNavigation>
     )
   }
 }
-
-Header.contextTypes = {
-	router: React.PropTypes.object
-};
 
 export default Header;
--- components/Product.js
+++ components/Product.js
@@ -17,7 +17,7 @@ export class Product extends Component {
             <img src={`http://s7d5.scene7.com/is/image/horizonhobby/${this.props.product.ProdID}_a0?wid=300&hei=300`} onError={(e) => this.errorHandler(e, 300)} className="thumbnail" alt="" />
           </Link>
         </p>
-        <p><strong>{this.props.product.Name.replace(/&reg;/g, '')}</strong><br />${this.props.product.Price}</p>
+        <p><strong>{`${this.props.product.Name.replace(/&reg;/g, '')} (${this.props.product.ProdID})`}</strong><br />${this.props.product.Price}</p>
       </div>
     )
   }
@@ -26,7 +26,7 @@ export class Product extends Component {
 export class ProdPageProduct extends Component {
   render() {
     return (
-      <div className="small-12">
+      <div className="small-12 columns">
         <div className="row">
           <div className="small-12 columns prodTitle">
             <h3>{this.props.product.Name.replace(/&reg;/g, '')}</h3><strong>{`(${this.props.product.ProdID})`}</strong>
