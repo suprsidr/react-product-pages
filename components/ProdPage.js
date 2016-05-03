45b9bfe34c45bca9b4a0c24388c0872124c7f6a9
suprsidr <suprsidr@gmail.com>
2016-05-02 19:57:35 -0500

    fetch data if product is not in local DB

--- components/ProdPage.js
+++ components/ProdPage.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {ProdPageProduct} from './Product';
+import request from 'superagent';
+import jsonp from 'superagent-jsonp';
 
 
 export default class ProdPage extends Component {
@@ -33,9 +35,62 @@ export default class ProdPage extends Component {
 	}
   query(q, p) {
     this.props.db.products.find(q, p).fetch((data) => {
-      this.setState({products: data});
+	    if(data.length === 0) {
+		    this.fetchData(this.props.params.id, (err, res) => {
+			    if(err) {
+				    console.log('Fetch data error: ', err, 'PartsList::query');
+			    } else {
+				    this.setState({products: res});
+			    }
+		    });
+	    } else {
+		    this.setState({products: data});
+	    }
     });
   }
+	fetchData(id, cb) {
+		const q = JSON.stringify({
+					Displayable: 1,
+					Buyable: 1,
+					ProdID: id
+				}),
+				s = JSON.stringify({
+					ProdID: 1
+				}),
+				f = JSON.stringify({
+					_id: 0,
+					ProdID: 1,
+					BrandName: 1,
+					Name: 1,
+					Desc: 1,
+					LongDesc: 1,
+					Price: 1,
+					ListPrice: 1,
+					PartsList: 1,
+					RatingAverage: 1,
+					RatingCount: 1,
+					Attributes: 1,
+					Categories: 1
+				});
+		request
+				.get(`http://159.203.116.76/search/${q}/0/${s}/${f}`)
+				.use(jsonp)
+				.end((err, res) => {
+					err ? console.log(err) : '';
+					//console.log(res.body);
+					this.upsert(res.body);
+					if(res.body.error) {
+						cb(res.body.error)
+					} else {
+						cb(null, res.body);
+					}
+				});
+	}
+	upsert(data) {
+		this.props.db.products.upsert(data, () => {
+			console.log('data upserted');
+		});
+	}
   render() {
     return (
       <div className="row">
