df8d6e2292585a9dc13f53b01d6941d9a67ad410
suprsidr <suprsidr@gmail.com>
2016-05-08 12:52:06 -0500

    not used

--- components/ProdImage.js
+++ components/ProdImage.js
@@ -1,5 +1,7 @@
+/**
+ * this component is not used and may be deleted in the future
+ */
 import React, {Component} from 'react';
-
 
 export default class ProdImage extends Component {
   render() {
