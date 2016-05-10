a51e9df603bc86ded25e7d4e2f4ded380ef4d4ec
suprsidr <suprsidr@gmail.com>
2016-05-10 07:37:31 -0500

    suppress compressor warnings

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,15 @@ const config = {
   plugins: [
 	  new webpack.optimize.DedupePlugin(),
 	  new webpack.optimize.OccurrenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({minimize: true}),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      compressor: {
+        warnings: false
+      }
+    }),
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': '"production"'
+        'NODE_ENV': JSON.stringify('production')
       }
     })
   ]
