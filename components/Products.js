860382d693500d9bf5a9c293a71bc21ad4291fa3
suprsidr <suprsidr@gmail.com>
2016-05-15 09:06:15 -0500

    display and handleFilterChange

--- components/Products.js
+++ components/Products.js
@@ -5,7 +5,10 @@ export default class Products extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      products: []
+      products: [],
+	    filters: {},
+	    baseQuery: {}, // the query you arrived on this page with
+	    currentFilterQuery: {} // the query we build from filters
     };
 	  this.fields = {
 		  ProdID: 1,
@@ -16,6 +19,7 @@ export default class Products extends Component {
 		  Categories: 1,
 		  _id: -1
 	  };
+	  this.selectedFilters = {};
   }
   componentWillMount() {
     //this.setState({category: this.props.params.category.toLowerCase()});
@@ -26,6 +30,37 @@ export default class Products extends Component {
   componentWillReceiveProps(nextProps) {
     this.makeQuery(nextProps.params.category.toLowerCase());
   }
+	handleFilterChange(e) {
+		e.preventDefault();
+		let query = {};
+		query['$and'] = Array.from(this.refs.filters.querySelectorAll('select')).reduce((prev, next) => {
+			let parts = next.value.split(':');
+			if(parts[1]) {
+				this.selectedFilters[parts[0]] = parts[1];
+				let obj = {
+					Attributes: {
+						$elemMatch: {
+							ID: parts[0],
+							Name: parts[1]
+						}
+					}
+				};
+				prev.push(obj);
+			} else {
+				delete this.selectedFilters[parts[0]];
+			}
+			return prev;
+		}, []);
+		// add our base query
+		query['$and'].unshift(this.state.baseQuery);
+		this.setState({currentFilterQuery: query}, () => this.query(query, {
+			limit: 0,
+			sort: {
+				DemandRank: 1
+			},
+			fields: this.fields
+		}));
+	}
   makeQuery(category) {
     let regex;
     switch(category) {
@@ -39,21 +74,23 @@ export default class Products extends Component {
 	      regex = /[A-Z]+_(RTF|BNF)$/;
 	      break;
     }
-    this.query({
-      Categories: {
-        $elemMatch: {
-          ID: {
-            $regex: regex
-          }
-        }
-      }
-    }, {
-      limit: 0,
-      sort: {
-	      DemandRank: 1
-      },
-      fields: this.fields
-    });
+	  let query = {
+		  Categories: {
+			  $elemMatch: {
+				  ID: {
+					  $regex: regex
+				  }
+			  }
+		  }
+	  };
+	  this.setState({baseQuery: query}, () => this.query(query, {
+		  limit: 0,
+		  sort: {
+			  DemandRank: 1
+		  },
+		  fields: this.fields
+	  }));
+
   }
   query(q, p) {
     this.props.db.products.find(q, p).fetch((data) => {
@@ -63,27 +100,44 @@ export default class Products extends Component {
 	getFilters() {
 		const filters = this.state.products.reduce((obj, product) => {
 			return product.Attributes.reduce((prev, next) => {
-				if(next.ID.includes('ZZ_')) return;
-				if(next.ID.includes('facet_')) {
-					let key = next.ID.replace(/(^spec_|facet_)/gi, '');
-					try{
-						prev[key].val.push(next.Name);
-					} catch(e) {
-						prev[key] = {};
-						prev[key].val = [];
-						prev[key].val.push(next.Name);
+				if(!next.ID.includes('ZZ_') && (next.ID.includes('facet_') || next.ID.includes('Completion_Level'))) {
+					let key = next.ID;
+					if(typeof prev[key] === 'undefined') {
+						prev[key] = [];
 					}
+					prev[key].push(next.Name);
 				}
 				return prev;
 			}, obj);
 		}, {});
-		console.log(filters);
+		this.setState({filters: filters});
 	}
   render() {
     return (
 	    <div className="row">
 		    <div className="small-12 columns">
 			    <h3 className="prodPageTitle">{this.props.params.category || `All Products`}</h3>
+			    <div ref="filters" className="row small-up-2 medium-up-3 large-up-4 callout secondary">
+				    {Object.keys(this.state.filters).sort((a, b) => a.replace(/(^spec_|facet_)/gi, '') > b.replace(/(^spec_|facet_)/gi, '')).map(key => {
+					    return (
+						    <div className="column">
+							    <strong>{key.replace(/(^spec_|facet_)/gi, '').replace(/_/g, ' ')}</strong>
+							    <select onChange={(e) => this.handleFilterChange(e)}>
+								    <option value={key} selected={!this.selectedFilters[key] ? 'selected': ''}>Please Select</option>
+								    {this.state.filters[key]
+										    .reduce((prev, next) => {
+											    if(prev.indexOf(next) === -1) {
+												    prev.push(next);
+											    }
+											    return prev;
+										    }, [])
+										    .sort((a, b) => a > b)
+										    .map((filter, i) => <option key={i} value={key +':'+ filter} selected={this.selectedFilters[key] === filter ? 'selected': ''}>{filter}</option>)}
+							    </select>
+						    </div>
+					    )
+				    })}
+			    </div>
 			    <div className="row small-up-2 medium-up-3 large-up-4">
 				    {this.state.products.map((product) => <Product key={product.ProdID} product={product} db={this.props.db} />)}
 			    </div>
