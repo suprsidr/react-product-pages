06265013749d41e6c7c40e26e1ca0672abbeaed4
suprsidr <suprsidr@gmail.com>
2016-05-22 12:36:14 -0500

    camelCase params

--- config/routes.js
+++ config/routes.js
@@ -11,7 +11,7 @@ import {Route, IndexRoute} from 'react-router';
 export default (
   <Route path="/" component={Main}>
     <Route path="/products/:category" component={Products} />
-    <Route path="/search/:searchterm" component={Search} />
+    <Route path="/search/:searchTerm" component={Search} />
     <Route path="/product/:id" component={ProdPage} />
     <Route path="/video" component={VideoPage} />
 	  <Route path="*" component={NotFound}/>
