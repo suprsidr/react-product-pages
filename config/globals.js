609ff633d3d44d8deb15db26f4a7df591f838512
suprsidr <suprsidr@gmail.com>
2016-05-22 12:38:24 -0500

    store discontinued image url

--- config/globals.js
+++ config/globals.js
@@ -1,7 +1,7 @@
 /**
  * Variables used globally
  */
-export const fetchUrl = 'http://159.203.116.76';
+export const fetchUrl = 'https://suprsidr.wtf';
 export const discImageUrl = 'http://s7d5.scene7.com/is/image/horizonhobby/4c_discFlag?fmt=png-alpha';
 
 
