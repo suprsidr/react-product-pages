3ab8d510f04ac42be90fc5b512e78c3a1ec4e892
suprsidr <suprsidr@gmail.com>
2016-05-22 19:29:33 -0500

    remove discImage

--- config/globals.js
+++ config/globals.js
@@ -2,7 +2,6 @@
  * Variables used globally
  */
 export const fetchUrl = 'https://suprsidr.wtf';
-export const discImageUrl = 'http://s7d5.scene7.com/is/image/horizonhobby/4c_discFlag?fmt=png-alpha';
 
 
 
