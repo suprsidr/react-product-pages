dc672bb6339fb1107cda2c467fc152554b4af7dd
suprsidr <suprsidr@gmail.com>
2016-11-23 07:57:06 -0500

    switch to api.hh.com

--- config/globals.js
+++ config/globals.js
@@ -1,7 +1,8 @@
 /**
  * Variables used globally
  */
-export const FETCH_URL = 'https://suprsidr.wtf';
+// export const FETCH_URL = 'https://suprsidr.wtf';
+export const FETCH_URL = 'https://api.horizonhobby.com';
 export const SCENE7_URL = 'https://s7d5.scene7.com/is/image/horizonhobby';
 
 
